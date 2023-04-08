b9d571d6a0f7501b9b2fa56cdf23a5815932c211
shautyfrfr <homeofmyths@gmail.com>
2023-04-08 04:51:05 -0400

    Patch
    Fixed minor irks

---
+++ wallpaperPtOS/Assets/scripts/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenLinkJSPlugin()
+	{
+		#if !UNITY_EDITOR
+		openWindow("http://www.mythicalgamers.com");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
